7cbffd11ed225c2ba1d2a7883c2a028da5ed53f6
itCatface <catface.wyh@foxmail.com>
2019-07-30 09:54:15 +0800

    test release

---
+++ ctool/src/main/java/cc/catface/ctool/TTT.java
@@ -0,0 +1,12 @@
+package cc.catface.ctool;
+
+import android.util.Log;
+
+/**
+ * Created by catfaceWYH --> tel|wechat|qq 130 128 92925
+ */
+public class TTT {
+    public static void t() {
+        Log.d("ccccc", "dadasfsa");
+    }
+}
