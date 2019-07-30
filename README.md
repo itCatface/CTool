004e3a6432f60a8b2f14cc8ec02d6e0d0c377673
itCatface <catface.wyh@foxmail.com>
2019-07-30 17:38:23 +0800

    test release

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CTool
-init.
+ItemClickSupport:专为RecyclerView的条目点击&长按设置的监听辅助类[ItemClickSupport.addTo(rv).setOnItemClickListener((recyclerView, position, v) -> {//...}).setOnItemLongClickListener((recyclerView, position, v) -> {//...    return false;});]
+CustomBindingAdapter:专为RecyclerView的Adapter设计，仅添加了DataBinding
---
+++ ctool/src/main/java/cc/catface/ctool/view/recyclerview/CustomBindingAdapter.java
@@ -0,0 +1,48 @@
+package cc.catface.ctool.view.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by catfaceWYH --> tel|wechat|qq 130 128 92925
+ */
+public abstract class CustomBindingAdapter<B extends ViewDataBinding> extends RecyclerView.Adapter<CustomBindingAdapter.Holder<B>> {
+    @NonNull @Override public Holder<B> onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        B binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), layoutId(), parent, false);
+        Holder<B> holder = new Holder<>(binding.getRoot());
+        holder.setBinding(binding);
+        return holder;
+    }
+
+    @Override public void onBindViewHolder(@NonNull Holder<B> holder, int position) {
+        onBindHolder(holder.getBinding(), position);
+    }
+
+
+    static class Holder<B> extends RecyclerView.ViewHolder {
+        private B binding;
+
+        Holder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        B getBinding() {
+            return binding;
+        }
+
+        void setBinding(B binding) {
+            this.binding = binding;
+        }
+    }
+
+
+    abstract int layoutId();
+
+    abstract void onBindHolder(B binding, int position);
+}
